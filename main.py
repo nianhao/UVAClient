b9b4b59c6b149400f3f460118de259579623c2d9
nianhao <1137136542@qq.com>
2019-02-25 15:05:46 +0800

    实现UDP信息的发送

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
 import sys
 
-from PyQt5 import uic, QtGui
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 
 qtCreatorFile="send.ui"
@@ -10,6 +11,26 @@ class MyApp(QWidget,Ui_MainWindow):
         QWidget.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
+
+        #绑定事件
+        self.eventBind()
+
+    #事件绑定
+    def eventBind(self):
+        self.pushButton_send.clicked.connect(self.sendMessage)
+    def sendMessage(self):
+        ip = self.lineEdit_IP.text()
+        message=self.textEdit_message.toPlainText()
+        print(ip,'\n',message)
+        ip,port=ip.split(':')
+        address=(ip,int(port))
+        print(address)
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.sendto(bytes(message,encoding="utf-8"),address)
+        except Exception as e:
+            print(e)
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
